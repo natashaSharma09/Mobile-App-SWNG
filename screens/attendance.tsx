aecd2a937d2ca62ba205adb5209f08b36bd6897b
natashaSharma09 <64687920+natashaSharma09@users.noreply.github.com>
2023-04-17 15:38:54 +1000

    Update attendance.tsx

--- screens/attendance.tsx
+++ screens/attendance.tsx
@@ -1,25 +1,55 @@
-
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView, } from 'react-native';
+import { StyleSheet, Image, TextInput, TouchableOpacity, ImageBackground, KeyboardAvoidingView } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 
-
-export default function AttendancePage() {
+export default function AttendancePage(props) {
+  const {
+    spanText1,
+    cellularConnection,
+    iconSignal,
+    cap,
+    swngLogoTransparentCmyk13,
+    unsplashJmurdhtm7Ng,
+    spanText2,
+    line2,
+    icons8ChatMessage5022,
+    icons8Checkmark3222,
+    iconHome1,
+    iconStar1,
+    spanText3,
+    spanText4,
+    spanText5,
+    spanText6,
+    spanText7,
+    spanText8,
+    spanText9,
+    spanText10,
+    spanText11,
+    spanText12,
+    spanText13,
+    spanText14,
+    spanText15,
+    rectangle431,
+    rectangle432,
+    icons8ChatMessage5024,
+    icons8Checkmark3224,
+    iconHome2,
+    iconStar2,
+    icons8ChatMessage5025,
+    icons8Checkmark3225,
+    iconHome3,
+    iconStar3
+  } = props;
 
   return (
     <View style={styles.container}>
-
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Image style  = {styles.image
-
-      } source={require('../assets//thumbnail_SWNG-white.png')} />
-      <Text> Attendance Page</Text>
-</View>
+      <Image style={styles.image} source={require('../assets/thumbnail_SWNG-white.png')} />
+      <Text>Attendance Page</Text>
+    </View>
   );
-
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -31,8 +61,7 @@ const styles = StyleSheet.create({
     marginBottom: '5%',
     resizeMode: 'contain',
     height: '20%',
-    width: '80%'
-    
+    width: '80%',
   },
   title: {
     fontSize: 20,
